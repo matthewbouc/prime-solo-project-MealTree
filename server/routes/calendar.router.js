9897a7367c50a27bd144c8b1683f03f8c7ac471b
Matthew Bouc <boucgm@gmail.com>
2021-08-03 16:50:52 -0500

    Calendar POST and PUT routes created and functional.  Tested in Postman

---
+++ server/routes/calendar.router.js
@@ -0,0 +1,76 @@
+const { default: axios } = require('axios');
+const express = require('express');
+const { rejectUnauthenticated } = require('../modules/authentication-middleware');
+const pool = require('../modules/pool');
+const router = express.Router();
+
+/**
+ * GET route template
+ */
+router.get('/', (req, res) => {
+  // GET route code here
+});
+
+/**
+ * POST to create a new calendar
+ */
+router.post('/', rejectUnauthenticated, (req, res) => {
+  const owner = req.user.id;
+  const calendarName = req.body.name;
+  console.log('req.user.id', owner);
+  const queryText = `INSERT INTO calendars (owner_id, name, shared_users) VALUES ($1,$2,$3);`;
+
+  pool.query(queryText, [owner, calendarName, `[${owner}]`])
+  .then(() => {
+    console.log('New Calendar created');
+    res.sendStatus(201);
+  }).catch(error => {
+    console.log ('New calendar failed', error);
+    res.sendStatus(500);
+  });
+});
+
+
+router.put('/:calendarId', rejectUnauthenticated, (req, res) => {
+  const calendarId = req.params.calendarId;
+  const requesterId = req.user.id;
+
+  if (req.body.username){
+    const addedUser = req.body.username;
+    console.log(`requesterId = ${requesterId}, --- addedUser = ${addedUser}
+                  ---- calendarId = ${calendarId}`);
+
+    const selectQuery = 'SELECT id FROM "user" WHERE username=$1;';
+    const putQuery = `UPDATE calendars SET shared_users = shared_users || $1
+                      WHERE owner_id = $2 AND id = $3;`
+    pool.query(selectQuery, [addedUser])
+    .then(response => {
+      const addedUserId = response.rows[0].id;
+      console.log('response is:', addedUserId);
+      pool.query(putQuery, [addedUserId, requesterId, calendarId])
+      .then(()=>{
+        console.log('Success PUTting shared_user');
+        res.sendStatus(202);
+      })
+    }).catch(error => {
+      console.log('error during overall put', error)
+      res.sendStatus(500);
+    })
+  } else if (req.body.calendarName){
+    const newName = req.body.calendarName;
+    console.log(newName);
+    
+    const queryText = `UPDATE calendars SET name = $1 WHERE owner_id = $2 AND id =$3;`;
+    pool.query(queryText, [newName, requesterId, calendarId])
+    .then(() => {
+      console.log('Success changing name');
+      res.sendStatus(202);
+    }).catch(error => {
+      console.log('Error changing calendar name', error);
+      res.sendStatus(500);
+    });
+  }
+});
+
+
+module.exports = router;
