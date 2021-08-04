8ea100f78a7d63205c336eabc12f43971a232df7
Matthew Bouc <boucgm@gmail.com>
2021-08-03 19:37:07 -0500

    Adding a table to ERD for shared users on calendar.  Commenting out most of PUT.  Reworking to a POST

--- server/routes/calendar.router.js
+++ server/routes/calendar.router.js
@@ -1,4 +1,3 @@
-const { default: axios } = require('axios');
 const express = require('express');
 const { rejectUnauthenticated } = require('../modules/authentication-middleware');
 const pool = require('../modules/pool');
@@ -18,9 +17,9 @@ router.post('/', rejectUnauthenticated, (req, res) => {
   const owner = req.user.id;
   const calendarName = req.body.name;
   console.log('req.user.id', owner);
-  const queryText = `INSERT INTO calendars (owner_id, name, shared_users) VALUES ($1,$2,$3);`;
+  const queryText = `INSERT INTO calendars (owner_id, name, shared_users) VALUES ($1,$2);`;
 
-  pool.query(queryText, [owner, calendarName, `[${owner}]`])
+  pool.query(queryText, [owner, calendarName])
   .then(() => {
     console.log('New Calendar created');
     res.sendStatus(201);
@@ -30,35 +29,14 @@ router.post('/', rejectUnauthenticated, (req, res) => {
   });
 });
 
-
+/**
+ * PUT routes to update calendar shared_users or calendar name
+ */
 router.put('/:calendarId', rejectUnauthenticated, (req, res) => {
   const calendarId = req.params.calendarId;
   const requesterId = req.user.id;
 
-  if (req.body.username){
-    const addedUser = req.body.username;
-    console.log(`requesterId = ${requesterId}, --- addedUser = ${addedUser}
-                  ---- calendarId = ${calendarId}`);
 
-    const selectQuery = 'SELECT id FROM "user" WHERE username=$1;';
-    const putQuery = `UPDATE calendars SET shared_users = shared_users || $1
-                      WHERE owner_id = $2 AND id = $3;`
-    pool.query(selectQuery, [addedUser])
-    .then(response => {
-      const addedUserId = response.rows[0].id;
-      console.log('response is:', addedUserId);
-      pool.query(putQuery, [addedUserId, requesterId, calendarId])
-      .then(()=>{
-        console.log('Success PUTting shared_user');
-        res.sendStatus(202);
-      })
-    }).catch(error => {
-      console.log('error during overall put', error)
-      res.sendStatus(500);
-    })
-  } else if (req.body.calendarName){
-    const newName = req.body.calendarName;
-    console.log(newName);
     
     const queryText = `UPDATE calendars SET name = $1 WHERE owner_id = $2 AND id =$3;`;
     pool.query(queryText, [newName, requesterId, calendarId])
@@ -69,8 +47,42 @@ router.put('/:calendarId', rejectUnauthenticated, (req, res) => {
       console.log('Error changing calendar name', error);
       res.sendStatus(500);
     });
-  }
 });
 
 
 module.exports = router;
+
+
+
+  // if (req.body.username){
+  //   const addedUser = req.body.username;
+  //   console.log(`requesterId = ${requesterId}, --- addedUser = ${addedUser}
+  //                 ---- calendarId = ${calendarId}`);
+
+  //   const selectQuery = 'SELECT id FROM "user" WHERE username=$1;';
+
+
+  //   const putQuery = `UPDATE calendars SET shared_users = shared_users || $1
+  //                     WHERE owner_id = $2 AND id = $3;`;
+  //   if (req.body.delete){
+  //     putQuery = `UPDATE calendars SET shared_users - $1
+  //                 WHERE owner_id = $2 AND id = $3;`;
+  //   }
+
+  //   pool.query(selectQuery, [addedUser])
+  //   .then(response => {
+  //     const addedUserId = response.rows[0].id;
+  //     console.log('response is:', addedUserId);
+  //     pool.query(putQuery, [addedUserId, requesterId, calendarId])
+  //     .then(()=>{
+  //       console.log('Success PUTting shared_user');
+  //       res.sendStatus(202);
+  //     })
+  //   }).catch(error => {
+  //     console.log('error during overall put', error)
+  //     res.sendStatus(500);
+  //   })
+  // } else if (req.body.calendarName){
+  //   const newName = req.body.calendarName;
+  //   console.log(newName);
+  // }
