fb8694559b6853403dc9471f9aff557b27ba516f
Matthew Bouc <boucgm@gmail.com>
2021-08-05 09:57:29 -0500

    POST new recipe route tested and operational

--- server/routes/recipes.router.js
+++ server/routes/recipes.router.js
@@ -33,5 +33,27 @@ router.get('/', rejectUnauthenticated, (req, res) => { //  QUERY!! (recipeId)
 });
 
 
+/**
+ * POST add new recipe to database
+ */
+router.post('/new', rejectUnauthenticated, (req, res) => {
+    const name = req.body.name;
+    const ingredients = req.body.ingredients;
+    const procedure = req.body.procedure;
+    const picture = req.body.picture;
+    const api_id = req.body.api_id; // THIS WILL NEED TO BE ADDRESSED ONCE API IS INTRODUCED.  Will have  to rework queryText
+
+    const queryText = `INSERT INTO recipes (name, ingredients, procedure, picture) VALUES ($1, $2, $3, $4);`;
+
+    pool.query(queryText, [name, ingredients, procedure, picture])
+    .then(() => {
+        console.log('Success POSTing new recipe');
+        res.sendStatus(201);
+    }).catch(error => {
+        console.log('Error POSTing new recipe', error);
+        res.sendStatus(500);
+    })
+})
+
 
 module.exports = router;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Header() {
+  return (
+    <div className="container">
+      <p>Header Here</p>
+    </div>
+  );
+}
+
+export default Header;
