345290790adeb5ce22ac125dfde92ec6663d8fcc
Matthew Bouc <boucgm@gmail.com>
2021-08-05 10:07:41 -0500

    updated POST route to also add recipe to owner's favorites

--- server/routes/recipes.router.js
+++ server/routes/recipes.router.js
@@ -43,17 +43,23 @@ router.post('/new', rejectUnauthenticated, (req, res) => {
     const picture = req.body.picture;
     const api_id = req.body.api_id; // THIS WILL NEED TO BE ADDRESSED ONCE API IS INTRODUCED.  Will have  to rework queryText
 
-    const queryText = `INSERT INTO recipes (name, ingredients, procedure, picture) VALUES ($1, $2, $3, $4);`;
+    const addRecipeQuery = `
+        WITH newRecipe as (
+            INSERT INTO recipes (name, ingredients, procedure, picture)
+            VALUES ($1, $2, $3, $4) RETURNING id
+        )
+        INSERT INTO users_recipes (owner_id, recipe_id) VALUES ($5, (SELECT id FROM newRecipe));`
+    ;
 
-    pool.query(queryText, [name, ingredients, procedure, picture])
+    pool.query(addRecipeQuery, [name, ingredients, procedure, picture, req.user.id])
     .then(() => {
         console.log('Success POSTing new recipe');
         res.sendStatus(201);
     }).catch(error => {
         console.log('Error POSTing new recipe', error);
         res.sendStatus(500);
-    })
-})
+    });
+});
 
 
 module.exports = router;
