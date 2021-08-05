792ecb2f336acb235985358e5ad52b35c87f01df
Matthew Bouc <boucgm@gmail.com>
2021-08-04 19:48:23 -0500

    PUT route for changing dates or changing category of a meal

--- server/routes/meal.plan.router.js
+++ server/routes/meal.plan.router.js
@@ -85,7 +85,7 @@ router.delete('/', rejectUnauthenticated, async (req,res) => { // QUERY
     const mealPlanId = req.query.mealPlan;
 
     const verifyUserQuery = `SELECT calendar_id FROM calendar_shared_users WHERE shared_user_id = $1;`;
-    const deleteQuery = `DELETE FROM meal_plan WHERE id = $1 AND calendar_id = $2;`;
+    const deleteQuery = `DELETE FROM meal_plan WHERE id = $1 AND calendar_id = $2;`;  // double verification to ensure correct row is deleted. (no maliciousness)
 
     await pool.query(verifyUserQuery, [req.user.id])
     .then(result => {
@@ -105,6 +105,52 @@ router.delete('/', rejectUnauthenticated, async (req,res) => { // QUERY
         .then(() => {
             console.log('Success DELETing');
             res.sendStatus(200);
+        }).catch(error => {
+            console.log('Error DELETing', error)
+            res.sendStatus(500);
+        });
+    }else {
+        res.sendStatus(403);
+    }
+});
+
+
+
+/**
+ * PUT to update meal_plan row
+ */
+router.put('/', rejectUnauthenticated, async (req, res) => { // BODY
+    let queryText;
+    if (req.body.category){
+        queryText = `UPDATE meal_plan SET category_id = $1 WHERE id = $2 AND calendar_id = $3;`; // verifies both are true to avoid malicious activity
+    } else if (req.body.date){
+        queryText = `UPDATE meal_plan SET date = $1 WHERE id = $2 AND calendar_id = $3;`; // verifies both are true to avoid malicious activity
+    }
+    const verifyUserQuery = `SELECT calendar_id FROM calendar_shared_users WHERE shared_user_id = $1;`;
+
+    const valueToChange = req.body.category || req.body.date;
+    const calendarId = req.body.calendarId;
+    const mealPlanId = req.body.mealPlanId;
+    let isVerified;
+
+    await pool.query(verifyUserQuery, [req.user.id])
+    .then(result => {
+        console.log('result rows:', result.rows);
+        for (calendar of result.rows){
+            if (calendar.calendar_id == calendarId){
+                isVerified = true;
+            }
+        }
+    }).catch(error => {
+        console.log('Error POSTing', error);
+        res.sendStatus(500);
+    });
+
+    if (isVerified){
+        pool.query(queryText, [valueToChange, mealPlanId, calendarId])
+        .then(() => {
+            console.log('Success DELETing');
+            res.sendStatus(202);
         }).catch(error => {
             console.log('Error DELETing', error)
             res.sendStatus(500);
