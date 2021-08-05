283e3dce0ad177d5cbbd29cad97f943c2709c1f5
Matthew Bouc <boucgm@gmail.com>
2021-08-05 13:48:45 -0500

    DELETE query functional and TESTED.  Removes recipe which cascades to users_recipes

--- server/routes/recipes.router.js
+++ server/routes/recipes.router.js
@@ -42,7 +42,7 @@ router.post('/new', rejectUnauthenticated, (req, res) => {
     const procedure = req.body.procedure;
     const picture = req.body.picture;
     const api_id = req.body.api_id; // THIS WILL NEED TO BE ADDRESSED ONCE API IS INTRODUCED.  Will have  to rework queryText
-
+    console.log(req.user.id);
     const addRecipeQuery = `
         WITH newRecipe as (
             INSERT INTO recipes (name, ingredients, procedure, picture)
@@ -57,6 +57,29 @@ router.post('/new', rejectUnauthenticated, (req, res) => {
         res.sendStatus(201);
     }).catch(error => {
         console.log('Error POSTing new recipe', error);
+        res.sendStatus(500);
+    });
+});
+
+/**
+ * DELETE a recipe from the database and from users_recipes (should be deleted ON CASCADE)
+ */
+router.delete('/:recipeId', rejectUnauthenticated, (req, res) => {
+    const recipeId = req.params.recipeId;
+    console.log(req.user.id);
+    const deleteQuery = `
+    DELETE FROM recipes USING users_recipes
+	    WHERE users_recipes.owner_id = $1 AND 
+        recipes.id = users_recipes.recipe_id AND users_recipes.recipe_id = $2;`
+    ; // EMULATE THIS!  This is a way to perform authentication without needing a multiple queries.  Go back and fix old routes when time permits.
+    // This query works but it always produces an 200 status, even if the user isn't allowed delete (and the delete doesn't occur)
+
+    pool.query(deleteQuery, [req.user.id, recipeId])
+    .then(() => {
+        console.log(`Success DELETing recipe`);
+        res.sendStatus(200);
+    }).catch(error => {
+        console.log('Error DELETing recipe', error);
         res.sendStatus(500);
     });
 });
