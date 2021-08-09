d4f4ac3ae3de0edac294fa5efd23d4e7af319df4
Matthew Bouc <boucgm@gmail.com>
2021-08-09 15:24:19 -0500

    POST route connected for adding new recipe.  basic DOM layout finished

--- src/components/NewRecipe/NewRecipe.jsx
+++ src/components/NewRecipe/NewRecipe.jsx
@@ -1,19 +1,43 @@
 import React, { useState } from 'react';
-import {useSelector} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import TextareaAutosize from '@material-ui/core/TextareaAutosize';
+import FormControl from '@material-ui/core/FormControl';
+import Box from '@material-ui/core/Box';
 
-// Basic functional component structure for React with default state
-// value setup. When making a new component be sure to replace the
-// component name TemplateFunction with the name for the new component.
-function NewRecipe(props) {
-  // Using hooks we're creating local state for a "heading" variable with
-  // a default value of 'Functional Component'
-  const store = useSelector((store) => store);
-  const [heading, setHeading] = useState('New Recipe');
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+      width: '25ch',
+    },
+  },
+}));
+
+
+function NewRecipe() {
+  const dispatch = useDispatch();
+  const classes = useStyles();
+  const [recipe, setRecipe] = useState({name: '', ingredients: '', procedure: '', picture: ''});
+  
+  const handleAddRecipe = () => {
+    console.log('recipe', recipe);
+    dispatch({type: 'ADD_NEW_RECIPE', payload: recipe})
+  }
 
   return (
-    <div>
-      <h2>{heading}</h2>
-    </div>
+    <Box>
+      <form className={classes.root} autoComplete="off" onSubmit={handleAddRecipe}>
+        <TextField required value={recipe.name} label="Recipe Name" variant="outlined" onChange={(event)=> setRecipe({...recipe, name: event.target.value})}/>
+        <TextareaAutosize required value={recipe.ingredients} aria-label="minimum height" minRows={8} placeholder="Ingredients" onChange={(event)=> setRecipe({...recipe, ingredients: event.target.value})} />
+        <TextareaAutosize required value={recipe.procedure} aria-label="minimum height" minRows={15} placeholder="Procedure" onChange={(event)=> setRecipe({...recipe, procedure: event.target.value})} />
+        <TextField value={recipe.picture} label="Picture URL - change to dropzone" variant="outlined" onChange={(event)=> setRecipe({...recipe, picture: event.target.value})}/>
+        <Button variant="contained" type="submit" >Add Recipe</Button>
+      </form>
+    </Box>
   );
 }
 
--- src/redux/sagas/recipe.saga.js
+++ src/redux/sagas/recipe.saga.js
@@ -4,16 +4,26 @@ import { call, put, takeEvery, takeLatest } from 'redux-saga/effects';
 
 function* recipeSaga() {
     yield takeLatest('GET_RECIPE_DETAILS', getRecipeDetails);
+    yield takeLatest('ADD_NEW_RECIPE', postRecipe);
 }
 
 // worker Saga: will be fired on "FETCH_USER" actions
 function* getRecipeDetails(action) {
     try {
-        console.log('actionpayload', action.payload)
+        console.log('actionpayload', action.payload);
         const recipeDetails = yield axios.get(`/api/recipes?recipeId=${action.payload}`);
         yield put({type: 'SET_RECIPE_DETAILS', payload: recipeDetails.data[0]});
     } catch (error) {
         console.log('Error GETting recipe details', error);
+    }
+}
+
+function* postRecipe(action){
+    try{
+        console.log('recipe payload', action.payload);
+        yield axios.post(`/api/recipes/new`, action.payload);
+    } catch(error) {
+        console.log('Error POSTing new recipe', error);
     }
 }
 
