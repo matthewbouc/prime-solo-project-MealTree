2e945ee73755176a55b7ef48edd4825b3eafcb5d
Matthew Bouc <boucgm@gmail.com>
2021-08-10 12:53:41 -0500

    styling on landing page.  background image appears, but limited to direct landing page return

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,24 +1,41 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import './LandingPage.css';
+import background from '../../images/blurLandingPage.jpeg';
+import { Container, Grid, makeStyles } from '@material-ui/core';
+
 
 // CUSTOM COMPONENTS
 import RegisterForm from '../RegisterForm/RegisterForm';
+import LoginForm from '../LoginForm/LoginForm';
+
+
+const useStyles = makeStyles((theme) => ({
+  backgroundImage: {
+    backgroundImage: `url(${background})`,
+    backgroundPosition: 'center', 
+    backgroundSize: 'cover', 
+    backgroundRepeat: 'no-repeat',
+    backgroundAttachment: 'fixed',
+  }
+}));
 
 function LandingPage() {
-  const [heading, setHeading] = useState('Welcome');
+  const classes = useStyles();
+  const [isRegistered, setIsRegistered] = useState(false);
   const history = useHistory();
 
   const onLogin = (event) => {
-    history.push('/login');
+    setIsRegistered(!isRegistered);
+    // history.push('/login');
   };
 
   return (
-    <div className="container">
-      <h2>{heading}</h2>
+    <>
+    <Grid className={classes.backgroundImage}>
 
-      <div className="grid">
-        <div className="grid-col grid-col_8">
+      <div >
+        <div >
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
             id felis metus. Vestibulum et pulvinar tortor. Morbi pharetra lacus
@@ -30,30 +47,9 @@ function LandingPage() {
             Quisque eget eros metus. Vestibulum bibendum fringilla nibh a
             luctus. Duis a sapien metus.
           </p>
-
-          <p>
-            Praesent consectetur orci dui, id elementum eros facilisis id. Sed
-            id dolor in augue porttitor faucibus eget sit amet ante. Nunc
-            consectetur placerat pharetra. Aenean gravida ex ut erat commodo, ut
-            finibus metus facilisis. Nullam eget lectus non urna rhoncus
-            accumsan quis id massa. Curabitur sit amet dolor nisl. Proin
-            euismod, augue at condimentum rhoncus, massa lorem semper lacus, sed
-            lobortis augue mi vel felis. Duis ultrices sapien at est convallis
-            congue.
-          </p>
-
-          <p>
-            Fusce porta diam ac tortor elementum, ut imperdiet metus volutpat.
-            Suspendisse posuere dapibus maximus. Aliquam vitae felis libero. In
-            vehicula sapien at semper ultrices. Vivamus sed feugiat libero. Sed
-            sagittis neque id diam euismod, ut egestas felis ultricies. Nullam
-            non fermentum mauris. Sed in enim ac turpis faucibus pretium in sit
-            amet nisi.
-          </p>
         </div>
-        <div className="grid-col grid-col_4">
-          <RegisterForm />
-
+        <div >
+          {isRegistered ? <LoginForm /> : <RegisterForm />}          
           <center>
             <h4>Already a Member?</h4>
             <button className="btn btn_sizeSm" onClick={onLogin}>
@@ -62,7 +58,8 @@ function LandingPage() {
           </center>
         </div>
       </div>
-    </div>
+    </Grid>
+      </>
   );
 }
 
