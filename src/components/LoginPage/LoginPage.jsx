8264b9710f8b043cd786c8cffbb093cd9e5223c9
Matthew Bouc <boucgm@gmail.com>
2021-08-10 20:06:49 -0500

    basic styling of register, login, landing page

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -1,15 +1,37 @@
 import React from 'react';
 import LoginForm from '../LoginForm/LoginForm';
 import { useHistory } from 'react-router-dom';
+import background from '../../images/login-register.jpeg';
+import { Button, Container, Grid, makeStyles, Typography } from '@material-ui/core';
+import Nav from '../Nav/Nav';
+
+const useStyles = makeStyles((theme) => ({
+  backgroundImage: {
+    backgroundImage: `url(${background})`,
+    backgroundPosition: 'center',
+    backgroundSize: 'cover', 
+    backgroundRepeat: 'no-repeat',
+    backgroundAttachment: 'fixed',
+    // width: '100vw',
+    height: '100vh',
+    paddingTop: '50px',
+  }
+}));
+
 
 function LoginPage() {
+  const classes = useStyles();
   const history = useHistory();
 
   return (
-    <div>
-      <LoginForm />
+    <>
+    <Nav />
 
-      <center>
+    <Grid container className={classes.backgroundImage} alignContent="center" justifyContent="center">
+    <Grid item xs={9} container justifyContent="center">
+    <LoginForm />
+    </Grid>
+    <Grid item xs={9} container justifyContent="center">
         <button
           type="button"
           className="btn btn_asLink"
@@ -19,8 +41,9 @@ function LoginPage() {
         >
           Register
         </button>
-      </center>
-    </div>
+      </Grid>
+      </Grid>
+      </>
   );
 }
 
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,50 +1,50 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import LogOutButton from '../LogOutButton/LogOutButton';
 import './Nav.css';
-import {useSelector} from 'react-redux';
 import mealTree from '../../images/mealTree.png';
+import { useHistory } from 'react-router-dom';
+import { Button, Grid } from '@material-ui/core';
 
 
 function Nav() {
-  const user = useSelector((store) => store.user);
+  const history = useHistory();
 
-  let loginLinkData = {
-    path: '/login',
-    text: 'Login / Register',
+
+
+  // may implement header functionality at a later point
+  // let loginLinkData = {
+  //   path: '/login',
+  //   text: 'Login / Register',
+  // };
+
+  // if (user.id != null) {
+  //   loginLinkData.path = '/calendar';
+  //   loginLinkData.text = 'Home';
+  // }
+
+  const onLogin = () => {
+    history.push('/login');
   };
 
-  if (user.id != null) {
-    loginLinkData.path = '/calendar';
-    loginLinkData.text = 'Home';
+  const onSignUp = () => {
+    history.push('/registration');
   }
 
   return (
 
-    <div className="front"><img src={mealTree} height="75px" />
-    <div className="back">
-    </div>
-    </div>
-    // <div className="nav">
-    //   {/* <Link to="/home">
-    //     <h2 className="nav-title">Prime Solo Project</h2>
-    //   </Link> */}
-    //   <div>
+    <Grid container className="background" justifyContent="center">
+      <Grid item xs={12} container justifyContent="center">
+      <img src={mealTree} height="45px" />
+      </Grid>
+      <Grid item xs={12} container justifyContent="center">
+      <Button color="primary" variant="contained" onClick={onLogin}>
+                Sign in
+      </Button>
+      <Button color="primary" variant="contained" onClick={onSignUp}>
+                Sign up
+      </Button>
+      </Grid>
 
-    //     {user.id && (
-    //       <>
-    //         <Link className="navLink" to="/info">
-    //           Info Page
-    //         </Link>
-    //         <LogOutButton className="navLink" />
-    //       </>
-    //     )}
-
-    //     <Link className="navLink" to="/about">
-    //       About
-    //     </Link>
-    //   </div>
-    // </div>
+    </Grid>
   );
 }
 
