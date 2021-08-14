d027d88e5f2862b97d9cc32bd0adced79019d519
Matthew Bouc <boucgm@gmail.com>
2021-08-14 06:26:26 -0500

    fixed error with menu and logout button - moved log out button functionality to FooterNav

--- src/components/CalendarList/CalendarList.jsx
+++ src/components/CalendarList/CalendarList.jsx
@@ -1,8 +1,25 @@
+import React, { useEffect, useState } from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import { useHistory } from 'react-router-dom';
+
+import '../App/App.css';
+
+
+
 import { Typography } from "@material-ui/core"
 
 function CalendarList () {
+    const history = useHistory();
+    const dispatch = useDispatch();
+
+    useEffect(()=>{
+        dispatch({type: 'GET_CALENDAR_LIST'})
+    },[])
+
     return(
+        <div className='standardBackground'>
         <Typography>CalendarList here</Typography>
+        </div>
     )
 }
 
